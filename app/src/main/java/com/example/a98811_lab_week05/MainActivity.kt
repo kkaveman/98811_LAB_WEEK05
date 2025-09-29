edb26b3c6257ac0b56fb9f1a2b1eeba0fa640e46
kkaveman <kevinanatta1@gmail.com>
2025-09-29 18:47:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a98811_lab_week05/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.a98811_lab_week05
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a98811_lab_week05.api.CatApiService
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        getCatImageResponse()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun getCatImageResponse(){
+        val call = catApiService.searchImages(1,"full")
+        call.enqueue(object: Callback<String>{
+            override fun onFailure(call: Call<String> ,t : Throwable){
+                Log.e(MAIN_ACTIVITY, "Failed to get response",t)
+            }
+
+            override fun onResponse(call : Call<String>, response: Response<String>){
+                if (response.isSuccessful){
+                    apiResponseView.text = response.body
+                }
+                else{
+                    Log.e(MAIN_ACTIVITY,"Failed to get response\n" + response.errorBody()?.string().orEmpty())
+                }
+            }
+        })
+    }
+
+    private val retrofit by lazy{
+        Retrofit.Builder()
+            .baseUrl("https//api.thecatapi.com/v1/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+
+    private val catApiService by lazy{
+        retrofit.create(CatApiService::class.java)
+    }
+
+    private val apiResponseView : TextView by lazy{
+        findViewById(R.id.api_response)
+    }
+
+    companion object{
+        const val  MAIN_ACTIVITY = "MAIN ACTIVITY"
+    }
+}
---
+++ app/src/main/java/com/example/a98811_lab_week05/api/CatApiService.kt
@@ -0,0 +1,14 @@
+package com.example.a98811_lab_week05.api
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface CatApiService {
+    @GET("images/search")
+    fun searchImages(
+        @Query("limit") limit:Int,
+        @Query("size") format: String
+
+    ): Call<String>
+}
